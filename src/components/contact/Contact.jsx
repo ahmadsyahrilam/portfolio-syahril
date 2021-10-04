30a40af26c16f94f72ac75da806a96b7a4550774
ahmadsyahrilam <ahmadsyahrilam@gmail.com>
2021-10-04 22:20:20 +0800

    import components to App.js

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,10 @@
+// import React from "react"; ---> if install and use extension ES7, no need to import React ...
+import "./contact.scss"
+
+export default function Contact(){
+    return (
+        <div className="contact">
+
+        </div>
+    )
+}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,1 +1,10 @@
 //use file name Topbar.js also can, but if use Topbar.jsx VS code will know we're working with ReactJS
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            
+        </div>
+    )
+}
