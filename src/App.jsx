87276ebe470d278926ae02824befa52a0207bde7
ahmadsyahrilam <ahmadsyahrilam@gmail.com>
2021-10-04 23:46:14 +0800

    update scss, add snapstyle, add file global.scss

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Works from "./components/works/Works"
 import Testimonials from "./components/testimonials/Testimonials"
 import Contact from "./components/contact/Contact"
 
+import "./app.scss"
+
 function App() {
   return (
     <div className="app">
