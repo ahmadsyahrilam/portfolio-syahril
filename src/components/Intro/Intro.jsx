2adce75f96c285260c478363ef73239c4f8abc00
ahmadsyahrilam <ahmadsyahrilam@gmail.com>
2021-10-05 10:19:54 +0800

    install ityped, update intro section

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,18 @@
 import "./intro.scss"
+import { init } from "ityped"
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: true, //show cursor typing
+            backDelay: 1500, //typing delay
+            backSpeed: 60,
+            strings: ['Programmer', 'Front-end Developer', 'Mobile Developer' ] }) //copy from ityped website
+    },[])
     return (
         <div className="intro" id="intro">
            <div className="left">
@@ -12,7 +24,7 @@ export default function Intro() {
                <div className="wrapper">
                    <h2>Hello ! I'm</h2>
                    <h1>Ahmad Syahril</h1>
-                   <h3>Developer</h3>
+                   <h3>Programmer <span ref={textRef}></span></h3>
                </div>
                <a href="#portfolio">
                    <img src="assets/down.png" alt="down" />
