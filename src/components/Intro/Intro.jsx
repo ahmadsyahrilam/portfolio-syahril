8092b234c644b207eb38fa03a475c86abb0ec425
ahmadsyahrilam <ahmadsyahrilam@gmail.com>
2021-10-05 09:47:35 +0800

    modifiy intro section, add image and arrow down

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-           
+           <div className="left">
+               <div className="imgContainer">
+                   <img src="assets/syahril.png" alt="syahril" />
+               </div>
+           </div>
+           <div className="right">
+               <div className="wrapper">
+                   <h2>Hello ! I'm</h2>
+                   <h1>Ahmad Syahril</h1>
+                   <h3>Developer</h3>
+               </div>
+               <a href="#portfolio">
+                   <img src="assets/down.png" alt="down" />
+               </a>
+           </div>
         </div>
     )
 }
